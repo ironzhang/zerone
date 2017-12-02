ac4549cf1cfe83d11ad0dd7c110599b96c5a5a3b
zhanghui <zhanghui@ablecloud.cn>
2017-12-02 20:43:32 +0800

    update server.go

--- rpc/server.go
+++ rpc/server.go
@@ -54,23 +54,25 @@ func (s *Server) register(rcvr interface{}, name string) error {
 	return nil
 }
 
-func (s *Server) serveCodec(c codec.ServerCodec) {
-	for {
-		response, req, method, rcvr, args, reply, err := s.readRequest(c)
-		if err != nil {
-			if response {
-				s.writeResponse(c, req, reply, err)
-			}
-		}
-
-		ctx := context.Background()
-		rets := method.Func.Call([]reflect.Value{rcvr, reflect.ValueOf(ctx), args, reply})
-		erri := rets[0].Interface()
-		if erri != nil {
-			err = erri.(error)
-		}
-		s.writeResponse(c, req, reply, err)
+func parseServiceMethod(serviceMethod string) (string, string, error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		return "", "", fmt.Errorf("service/method request ill-formed: %s", serviceMethod)
 	}
+	return serviceMethod[:dot], serviceMethod[dot+1:], nil
+}
+
+func (s *Server) lookupServiceMethod(serviceName, methodName string) (reflect.Value, *method, error) {
+	svci, ok := s.serviceMap.Load(serviceName)
+	if !ok {
+		return reflect.Value{}, nil, fmt.Errorf("can't find service %s.%s", serviceName, methodName)
+	}
+	svc := svci.(*service)
+	meth, ok := svc.methods[methodName]
+	if !ok {
+		return reflect.Value{}, nil, fmt.Errorf("can't find method %s.%s", serviceName, methodName)
+	}
+	return svc.rcvr, meth, nil
 }
 
 func (s *Server) readRequest(c codec.ServerCodec) (response bool, req serverRequest, method reflect.Method, rcvr, args, reply reflect.Value, err error) {
@@ -138,23 +140,21 @@ func (s *Server) writeResponse(c codec.ServerCodec, req serverRequest, reply int
 	return c.WriteResponse(&resp, reply)
 }
 
-func (s *Server) lookupServiceMethod(serviceName, methodName string) (reflect.Value, *method, error) {
-	svci, ok := s.serviceMap.Load(serviceName)
-	if !ok {
-		return reflect.Value{}, nil, fmt.Errorf("can't find service %s.%s", serviceName, methodName)
-	}
-	svc := svci.(*service)
-	meth, ok := svc.methods[methodName]
-	if !ok {
-		return reflect.Value{}, nil, fmt.Errorf("can't find method %s.%s", serviceName, methodName)
-	}
-	return svc.rcvr, meth, nil
-}
+func (s *Server) serveCodec(c codec.ServerCodec) {
+	for {
+		response, req, method, rcvr, args, reply, err := s.readRequest(c)
+		if err != nil {
+			if response {
+				s.writeResponse(c, req, reply, err)
+			}
+		}
 
-func parseServiceMethod(serviceMethod string) (string, string, error) {
-	dot := strings.LastIndex(serviceMethod, ".")
-	if dot < 0 {
-		return "", "", fmt.Errorf("service/method request ill-formed: %s", serviceMethod)
+		ctx := context.Background()
+		rets := method.Func.Call([]reflect.Value{rcvr, reflect.ValueOf(ctx), args, reply})
+		erri := rets[0].Interface()
+		if erri != nil {
+			err = erri.(error)
+		}
+		s.writeResponse(c, req, reply, err)
 	}
-	return serviceMethod[:dot], serviceMethod[dot+1:], nil
 }
