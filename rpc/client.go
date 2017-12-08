71d9c71edb370b4398640021b6534d145834a4a9
zhanghui <zhanghui@ablecloud.cn>
2017-12-08 21:20:26 +0800

    update rpc client

--- rpc/client.go
+++ rpc/client.go
@@ -1,10 +1,48 @@
 package rpc
 
-import "context"
+import (
+	"context"
+	"sync"
 
-type Client struct {
+	"github.com/ironzhang/zerone/rpc/codec"
+)
+
+type Call struct {
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
 }
 
-func (c *Client) Call(ctx context.Context, method string, args interface{}, reply interface{}) error {
+type Client struct {
+	codec    codec.ClientCodec
+	pending  sync.Map
+	sequence uint64
+}
+
+func NewClientWithCodec(c codec.ClientCodec) *Client {
+	client := &Client{codec: c}
+	go client.reading()
+}
+
+func (c *Client) reading() {
+	//	var err error
+	//	for {
+	//		var resp codec.ResponseHeader
+	//		if err = c.codec.ReadResponseHeader(&resp); err != nil {
+	//			break
+	//		}
+	//		c.pending.Load(resp.Sequence)
+	//	}
+}
+
+func (c *Client) send(call *Call) error {
+}
+
+func (c *Client) Go(ctx context.Context, serviceMethod string, args interface{}, reply interface{}, done chan *Call) error {
+	return nil
+}
+
+func (c *Client) Call(ctx context.Context, serviceMethod string, args interface{}, reply interface{}) error {
 	return nil
 }
