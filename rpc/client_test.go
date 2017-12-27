d2b20b3b5fdf8d00954020612e4b3ebebdaa0c60
zhanghui <zhanghui@ablecloud.cn>
2017-12-27 12:42:32 +0800

    update client test

--- rpc/client_test.go
+++ rpc/client_test.go
@@ -3,12 +3,14 @@ package rpc
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"net"
 	"reflect"
 	"testing"
 
 	"github.com/ironzhang/zerone/rpc/codec"
 	"github.com/ironzhang/zerone/rpc/codec/json-codec"
+	"github.com/ironzhang/zerone/rpc/codes"
 )
 
 type testClientCodec struct {
@@ -39,21 +41,115 @@ func (c *testClientCodec) ReadResponseBody(a interface{}) error {
 	if c.respBodyErr != nil {
 		return c.respBodyErr
 	}
+	if a == nil {
+		return nil
+	}
 	data, err := json.Marshal(c.respBody)
 	if err != nil {
 		return err
 	}
-	if err = json.Unmarshal(data, a); err != nil {
-		return err
-	}
-	return nil
+	return json.Unmarshal(data, a)
 }
 
 func (c *testClientCodec) Close() error {
 	return nil
 }
 
-func TestClientGo(t *testing.T) {
+func TestClientReadResponseCorrect(t *testing.T) {
+	tests := []struct {
+		header codec.ResponseHeader
+		body   interface{}
+		reply  interface{}
+	}{
+		{
+			header: codec.ResponseHeader{ServiceMethod: "Arith.Add", Sequence: 1},
+			body:   &Reply{3},
+			reply:  &Reply{},
+		},
+		{
+			header: codec.ResponseHeader{ServiceMethod: "Arith.Div", Sequence: 1},
+			body:   &Reply{3},
+			reply:  &Reply{},
+		},
+	}
+
+	for i, tt := range tests {
+		clientCodec := &testClientCodec{respHeader: tt.header, respBody: tt.body}
+		client := Client{codec: clientCodec}
+		call := &Call{
+			Reply: tt.reply,
+			Done:  make(chan *Call, 1),
+		}
+		client.pending.Store(tt.header.Sequence, call)
+		if err := client.readResponse(); err != nil {
+			t.Fatalf("case%d: read response: %v", i, err)
+		}
+		<-call.Done
+		if got, want := tt.reply, tt.body; !reflect.DeepEqual(got, want) {
+			t.Fatalf("case%d: reply: %v != %v", i, got, want)
+		}
+		t.Logf("case%d: reply=%v", i, tt.reply)
+	}
+}
+
+func TestClientReadResponseError(t *testing.T) {
+	tests := []struct {
+		headerErr error
+		header    codec.ResponseHeader
+		bodyErr   error
+		expectErr error
+	}{
+		{
+			headerErr: io.EOF,
+			expectErr: io.EOF,
+		},
+		{
+			header:    codec.ResponseHeader{ServiceMethod: "Arith.Add", Sequence: 1},
+			bodyErr:   io.ErrUnexpectedEOF,
+			expectErr: io.ErrUnexpectedEOF,
+		},
+		{
+			header: codec.ResponseHeader{
+				ServiceMethod: "Arith.Add",
+				Sequence:      1,
+				Error: codec.Error{
+					Code:  int(codes.Internal),
+					Desc:  codes.Internal.String(),
+					Cause: "cause",
+				},
+			},
+			expectErr: nil,
+		},
+	}
+
+	for i, tt := range tests {
+		clientCodec := &testClientCodec{respHeaderErr: tt.headerErr, respHeader: tt.header, respBodyErr: tt.bodyErr}
+		client := Client{codec: clientCodec}
+		call := &Call{
+			Done: make(chan *Call, 1),
+		}
+		client.pending.Store(tt.header.Sequence, call)
+		err := client.readResponse()
+		if err != tt.expectErr {
+			t.Fatalf("case%d: read response expect %v but got %v", i, tt.expectErr, err)
+		}
+		if err != nil {
+			t.Logf("case%d: read response: %v", i, err)
+		}
+		select {
+		case <-call.Done:
+			t.Logf("case%d: error=%v", i, call.Error)
+		default:
+		}
+	}
+}
+
+func CodecPipe() (codec.ClientCodec, codec.ServerCodec) {
+	c, s := net.Pipe()
+	return json_codec.NewClientCodec(c), json_codec.NewServerCodec(s)
+}
+
+func TestClientCall(t *testing.T) {
 	tests := []struct {
 		serviceMethod string
 		args          interface{}
@@ -66,25 +162,34 @@ func TestClientGo(t *testing.T) {
 			reply:         &Reply{},
 			result:        &Reply{3},
 		},
+		{
+			serviceMethod: "Arith.Div",
+			args:          Args{10, 2},
+			reply:         &Reply{},
+			result:        &Reply{5},
+		},
 	}
 
-	cli, svr := net.Pipe()
-	c := NewClient(cli)
-	s := json_codec.NewServerCodec(svr)
+	clientCodec, serverCodec := CodecPipe()
+	go func() {
+		var req codec.RequestHeader
+		var resp codec.ResponseHeader
+		for _, tt := range tests {
+			serverCodec.ReadRequestHeader(&req)
+			serverCodec.ReadRequestBody(nil)
+			resp.ServiceMethod = req.ServiceMethod
+			resp.Sequence = req.Sequence
+			serverCodec.WriteResponse(&resp, tt.result)
+		}
+	}()
+	client := NewClientWithCodec(clientCodec)
 	for _, tt := range tests {
-		call, err := c.Go(context.Background(), tt.serviceMethod, tt.args, tt.reply, nil)
-		if err != nil {
+		if err := client.Call(context.Background(), tt.serviceMethod, tt.args, tt.reply); err != nil {
 			t.Fatalf("call %q: %v", tt.serviceMethod, err)
 		}
-		h := codec.ResponseHeader{
-			ServiceMethod: tt.serviceMethod,
-			Sequence:      1,
-		}
-		s.WriteResponse(&h, tt.result)
-		<-call.Done
-
 		if got, want := tt.reply, tt.result; !reflect.DeepEqual(got, want) {
-			t.Fatalf("reply: %v != %v", got, want)
+			t.Fatalf("call %q reply: %v != %v", tt.serviceMethod, got, want)
 		}
+		t.Logf("call %q success, args=%v, reply=%v", tt.serviceMethod, tt.args, tt.reply)
 	}
 }
