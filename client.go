da46b1d628fe92c1ef0fbe6f87e9eecaac97e390
zhanghui <zhanghui@ablecloud.cn>
2018-04-24 21:03:57 +0800

    update zerone

--- client.go
+++ client.go
@@ -1,16 +1,64 @@
 package zerone
 
-import "context"
+import (
+	"context"
+
+	"github.com/ironzhang/zerone/route"
+	"github.com/ironzhang/zerone/route/balance"
+)
+
+type LoadBalancer string
+
+const (
+	HashBalancer       LoadBalancer = "HashBalancer"
+	RandomBalancer     LoadBalancer = "RandomBalancer"
+	RoundRobinBalancer LoadBalancer = "RoundRobinBalancer"
+)
 
 type Client struct {
+	table              route.Table
+	balancer           route.LoadBalancer
+	hashBalancer       *balance.HashBalancer
+	randomBalancer     *balance.RandomBalancer
+	roundRobinBalancer *balance.RoundRobinBalancer
+}
+
+func NewClient(table route.Table) *Client {
+	c := &Client{
+		table:              table,
+		hashBalancer:       balance.NewHashBalancer(table, nil),
+		randomBalancer:     balance.NewRandomBalancer(table),
+		roundRobinBalancer: balance.NewRoundRobinBalancer(table),
+	}
+	c.balancer = c.randomBalancer
+	return c
 }
 
 func (c *Client) Close() error {
 	return nil
 }
 
+func (c *Client) getLoadBalancer(lb LoadBalancer) route.LoadBalancer {
+	switch lb {
+	case HashBalancer:
+		return c.hashBalancer
+	case RandomBalancer:
+		return c.randomBalancer
+	case RoundRobinBalancer:
+		return c.roundRobinBalancer
+	default:
+		return c.randomBalancer
+	}
+}
+
 func (c *Client) WithLoadBalancer(lb LoadBalancer) *Client {
-	return &Client{}
+	return &Client{
+		table:              c.table,
+		balancer:           c.getLoadBalancer(lb),
+		hashBalancer:       c.hashBalancer,
+		randomBalancer:     c.randomBalancer,
+		roundRobinBalancer: c.roundRobinBalancer,
+	}
 }
 
 func (c *Client) WithFailPolicy(fp FailPolicy) *Client {
