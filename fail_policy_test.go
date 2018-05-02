f26031557871083645acb0ed5e0da6e7f6341229
zhanghui <zhanghui@ablecloud.cn>
2018-05-02 10:54:55 +0800

    add test case for fail policy

--- fail_policy_test.go
+++ fail_policy_test.go
@@ -160,3 +160,86 @@ func TestFailtry(t *testing.T) {
 		}
 	}
 }
+
+func TestFailover(t *testing.T) {
+	tb := stable.NewTable([]route.Endpoint{
+		{"0", "tcp", "localhost:10000", 0},
+		{"1", "tcp", "localhost:10001", 0},
+	})
+
+	var (
+		docnt int
+		addrs []string
+	)
+
+	tests := []struct {
+		try   int
+		err   error
+		docnt int
+		addrs []string
+	}{
+		{
+			try:   0,
+			err:   nil,
+			docnt: 1,
+			addrs: []string{
+				"tcp://localhost:10000",
+			},
+		},
+		{
+			try:   2,
+			err:   nil,
+			docnt: 1,
+			addrs: []string{
+				"tcp://localhost:10000",
+			},
+		},
+		{
+			try:   0,
+			err:   io.EOF,
+			docnt: 1,
+			addrs: []string{
+				"tcp://localhost:10000",
+			},
+		},
+		{
+			try:   2,
+			err:   io.EOF,
+			docnt: 2,
+			addrs: []string{
+				"tcp://localhost:10000",
+				"tcp://localhost:10001",
+			},
+		},
+		{
+			try:   3,
+			err:   io.EOF,
+			docnt: 3,
+			addrs: []string{
+				"tcp://localhost:10000",
+				"tcp://localhost:10001",
+				"tcp://localhost:10000",
+			},
+		},
+	}
+	for i, tt := range tests {
+		docnt = 0
+		addrs = nil
+		do := func(net, addr string) (*rpc.Call, error) {
+			docnt++
+			addrs = append(addrs, fmt.Sprintf("%s://%s", net, addr))
+			return nil, tt.err
+		}
+
+		lb := balance.NewRoundRobinBalancer(tb)
+		f := NewFailover(tt.try)
+		f.execute(lb, nil, do)
+
+		if got, want := docnt, tt.docnt; got != want {
+			t.Errorf("%d: docnt: %v != %v", i, got, want)
+		}
+		if got, want := addrs, tt.addrs; !reflect.DeepEqual(got, want) {
+			t.Errorf("%d: addrs: %v != %v", i, got, want)
+		}
+	}
+}
